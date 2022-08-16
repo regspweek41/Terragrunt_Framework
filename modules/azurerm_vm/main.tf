50121f68f9c41204ece7192cce06d3b8f7de1873
DInesh Reddy Jetti <DRJetti@zuelligpharma.com>
2022-08-16 21:11:23 +0530

    test

--- modules/azurerm_vm/main.tf
+++ modules/azurerm_vm/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = var.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefix     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
