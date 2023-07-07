2e3598a5ebae7fdf634b47fecb09d3f51dd6cfbf
Ubuntu <azureuser@ado-201-vm.hrtmscrxdnpujdyoshfsq1hfza.ix.internal.cloudapp.net>
2023-07-07 16:11:03 +0000

    aks

---
+++ modules/aks/variables.tf
@@ -0,0 +1,14 @@
+variable "aks_name" {
+  type        = string
+  description = "key vault name"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "resource_group_name"
+}
+
+variable "location" {
+  type        = string
+  description = "location"
+}
